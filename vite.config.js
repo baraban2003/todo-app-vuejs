996c93a41bf594f0395dfc483943bd9f293dd50b
Volodymyr Baraban <barabanv2003@gmail.com>
2025-06-09 11:05:09 +0300

    base fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/todo-app/',
+  base: '/todo-app-vuejs/',
   plugins: [
     vue(),
     vueDevTools(),
